fa818a4753b066ce77d95d0e3db984670bed05ea
RaihanKabir277 <raihank0198@gmail.com>
2025-06-04 07:10:45 +0600

    complete the game with raw code next modify this game

--- game.py
+++ game.py
@@ -7,26 +7,51 @@ print("\n welcome to the higher and lower game \n")
 
 length = len(game_data.data)
 
-item = random.randint(0, length - 1)
+def game_play():
+    count = 0
 
-print(f"\n Compare A : {game_data.data[item]["name"]}, {game_data.data[item]["description"]}, From {game_data.data[item]["country"]}")
+    while True:
 
-print("\n\n VS \n\n")
+       
+        item = random.randint(0, length - 1)
+        print(f"\n Compare A : {game_data.data[item]["name"]}, {game_data.data[item]["description"]}, From {game_data.data[item]["country"]}")
 
-item1 = random.randint(0, length - 1)
+        print("\n\n VS \n\n")
 
-print(f"\n Compare B : {game_data.data[item1]["name"]}, {game_data.data[item1]["description"]}, From {game_data.data[item1]["country"]}")
+        item1 = random.randint(0, length - 1)
 
-follower_A = (game_data.data[item]["follower_count"])
-follower_B = (game_data.data[item1]["follower_count"])
+        print(f" Compare B : {game_data.data[item1]["name"]}, {game_data.data[item1]["description"]}, From {game_data.data[item1]["country"]}")
+
+        follower_A = (game_data.data[item]["follower_count"])
+        follower_B = (game_data.data[item1]["follower_count"])
 
 
-compare = input("Who has more followers on instragram? type 'A' or 'B' : " ).lower()
+        compare = input("Who has more followers on instragram? type 'A' or 'B' : " ).lower()
+        
+        if compare == "a" and follower_A > follower_B :
+            print("\n" * 20)
+            count += 1
+            print(f"you are right, Your current score is = {count}")
 
-if compare == "a" and follower_A > follower_B :
-    print("\n" * 20)
-    print("you are right")
+        elif compare == "b" and follower_B > follower_A :
+            print("\n" * 20)
+            count += 1
+            print(f"you are right, Your current score is = {count}")
 
-else:
-    print("\n" * 20)
-    print("You are wrong")
+        elif compare == "a" and follower_A < follower_B :
+            print("\n" * 20)
+            print(f"you are wrong, Your final score is = {count}")
+            break
+
+        elif compare == "b" and follower_B < follower_A :
+            print("\n" * 20)
+            print(f"you are wrong, Your final score is = {count}")
+            break
+
+
+        
+
+game_play()
+        
+
+    
